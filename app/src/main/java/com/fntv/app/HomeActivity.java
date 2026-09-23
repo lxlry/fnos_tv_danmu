67c8ec34b0fe023af7e26790af10bbf034d3ad5c
lxlry <18210855651@163.com>
2026-09-23 12:24:52 +0800

    update

--- app/src/main/java/com/fntv/app/HomeActivity.java
+++ app/src/main/java/com/fntv/app/HomeActivity.java
@@ -14,6 +14,7 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
